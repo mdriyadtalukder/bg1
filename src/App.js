2c398b2e741b230866d1b17672c8ff85582d2183
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-08-07 22:38:12 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <div className='one'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis impedit cumque maiores illo, ipsam omnis, dolor ut voluptate laudantium, aperiam sint. Excepturi voluptatum repellendus consectetur quos, perferendis eaque sed incidunt iusto ab ipsa. Aliquam obcaecati delectus eum eius dicta enim praesentium a dolorum neque suscipit maiores harum cum, velit rem dolore soluta, quisquam ea numquam ipsam. Voluptate nisi reprehenderit sed nemo voluptatibus labore laudantium perferendis accusantium aspernatur, repudiandae commodi necessitatibus, illum atque! Numquam omnis quos vel consectetur accusantium minus quo accusamus hic deleniti officiis culpa eveniet voluptates dicta praesentium tempore, quod necessitatibus commodi error fugit quisquam fuga officia natus harum impedit? Assumenda inventore cumque esse repudiandae sunt optio debitis. Dolores porro quaerat voluptas dolore, itaque laborum quis, sapiente incidunt ipsa perspiciatis ea quidem, veniam facere deleniti nemo non suscipit provident cumque pariatur quam? Porro saepe quas dolores necessitatibus minima pariatur quasi incidunt eveniet impedit velit natus asperiores atque, debitis unde quod? Dolorum quisquam tempore voluptatum explicabo ullam, nostrum debitis facilis. Totam facere eum quos excepturi? Sunt error, adipisci fuga magni voluptas nisi accusantium quis et recusandae reprehenderit nam minus, fugit enim quam minima explicabo iure natus nulla. Velit sed qui officia quia neque quasi facilis error laudantium, aspernatur culpa quas enim similique consequatur deleniti illo aliquid ratione odit?</p>
+        </div>
+      </div>
+      <div className="ap">
+        <div className='two'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis impedit cumque maiores illo, ipsam omnis, dolor ut voluptate laudantium, aperiam sint. Excepturi voluptatum repellendus consectetur quos, perferendis eaque sed incidunt iusto ab ipsa. Aliquam obcaecati delectus eum eius dicta enim praesentium a dolorum neque suscipit maiores harum cum, velit rem dolore soluta, quisquam ea numquam ipsam. Voluptate nisi reprehenderit sed nemo voluptatibus labore laudantium perferendis accusantium aspernatur, repudiandae commodi necessitatibus, illum atque! Numquam omnis quos vel consectetur accusantium minus quo accusamus hic deleniti officiis culpa eveniet voluptates dicta praesentium tempore, quod necessitatibus commodi error fugit quisquam fuga officia natus harum impedit? Assumenda inventore cumque esse repudiandae sunt optio debitis. Dolores porro quaerat voluptas dolore, itaque laborum quis, sapiente incidunt ipsa perspiciatis ea quidem, veniam facere deleniti nemo non suscipit provident cumque pariatur quam? Porro saepe quas dolores necessitatibus minima pariatur quasi incidunt eveniet impedit velit natus asperiores atque, debitis unde quod? Dolorum quisquam tempore voluptatum explicabo ullam, nostrum debitis facilis. Totam facere eum quos excepturi? Sunt error, adipisci fuga magni voluptas nisi accusantium quis et recusandae reprehenderit nam minus, fugit enim quam minima explicabo iure natus nulla. Velit sed qui officia quia neque quasi facilis error laudantium, aspernatur culpa quas enim similique consequatur deleniti illo aliquid ratione odit?</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
